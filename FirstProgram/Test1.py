b8f5b3fca71517c5541c8ec7306201ee71e9a27f
easdale2010 <easdale2010@hotmail.co.uk>
2017-10-15 17:58:28 +0100

    First Program

---
+++ FirstProgram/Test1.py
@@ -0,0 +1,29 @@
+import random
+import  sys
+import os
+
+print("Hello World")
+
+name = "Derek"
+print(name)
+
+print ("5 + 2 = ", 5+2)
+print ("5 - 2 = ", 5-2)
+print ("5 * 2 = ", 5*2)
+print ("5 / 2 = ", 5/2)
+print ("5 % 2 = ", 5%2)
+print ("5 ** 2 = ", 5**2)
+print ("5 // 2 = ", 5//2)
+
+
+quote = "\"Alays remember you are unique"
+multi_line_quote = '''Just
+like everyone else'''
+
+new_string = quote + multi_line_quote
+
+print(quote)
+
+print (multi_line_quote)
+
+print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
